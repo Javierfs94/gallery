48dc61ec17ba1a358e35e1412455cfac5882b3aa
Javierfs <javifs94@gmail.com>
2023-03-09 19:46:28 +0100

    Added site links

--- admin/comments.php
+++ admin/comments.php
@@ -1,9 +1,25 @@
+
+<?php include("includes/header.php"); ?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+    <?php include("includes/top_nav.php"); ?>
+
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+    <?php include("includes/side_nav.php"); ?>
+
+    <!-- /.navbar-collapse -->
+</nav>
+
+<div id="page-wrapper">
+
 <div class="container-fluid">
     <!-- Page Heading -->
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">
-                Blank Page
+                Comments
                 <small>Subheading</small>
             </h1>
             <ol class="breadcrumb">
@@ -18,3 +34,9 @@
     </div>
     <!-- /.row -->
 </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
---
+++ admin/includes/database.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("config.php");
+
+class Database
+{
+
+    private $connection;
+
+
+    public function open_db_connection()
+    {
+        $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        if (mysqli_connect_errno()) {
+            die("Database connection failed badly");
+        }
+    }
+}
+
+$database = new Database();
+$database->open_db_connection();
