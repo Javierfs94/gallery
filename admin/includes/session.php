8aa3c927d67db00bad4b29242f1a08d415348916
Javierfs <javifs94@gmail.com>
2023-03-28 19:58:30 +0200

    Added login and logout to Session class

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -10,6 +10,29 @@ class Session
     function __construct()
     {
         session_start();
+        $this->check_the_login();
+    }
+
+
+
+    public function is_signed_in()
+    {
+        return $this->signed_in;
+    }
+
+    public function login($user)
+    {
+        if ($user) {
+            $this->user_id = $_SESSION['user_id'] = $user->id;
+            $this->signed_in = true;
+        }
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        unset($this->user_id);
+        $this->signed_in = false;
     }
 
 
